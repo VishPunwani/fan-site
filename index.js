191858a4ac4b63a1e8943d6c689dc33bbe35f77b
vishnu-punwani12 <vishnupunwani20@gmail.com>
2023-02-14 16:05:54 -0800

    navbar-fixing

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function myFunction() {
-    var x = document.getElementById("navbar");
-    if (x.className === "navbar") {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
       x.className += " responsive";
     } else {
-      x.className = "navbar";
+      x.className = "topnav";
     }
   }
