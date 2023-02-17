ebb240149578ef7f44c7a1bd5faa99a419044bb7
vishnu-punwani12 <vishnupunwani20@gmail.com>
2023-02-16 21:29:57 -0800

    complete header redo

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+const theBody = document.querySelector('body');
+const openNav = document.querySelector('.menu-bar button');
+const closeNav = document.querySelector('.close-nav button');
+const Navbar = document.querySelector('.navbar');
+
+function showHide(){
+    Navbar.classList.toggle('show');
+}
+
+openNav.onclick = showHide;
+closeNav.onclick = showHide;
